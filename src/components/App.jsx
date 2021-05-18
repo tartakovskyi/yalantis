8ff23df59be811fbcb06f80843a7bc06edb9443c
tartakovskyi <vvt2001@ukr.net>
2021-05-18 16:58:42 +0300

    start2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,24 @@
 import React  from 'react';
 
 
+export const AppContext = React.createContext();
+
 const App = () => {
 
   return (
-    <h1>test</h1>
+
+    <AppContext.Provider value={} >
+      <header>
+        <div className="container">
+          <a href="/">Yalantis React School</a>
+        </div>  
+      </header>
+      <main>
+        <div className="container">
+        <h1>Test</h1>
+        </div>
+      </main>
+    </AppContext.Provider>
   )
 }
 
