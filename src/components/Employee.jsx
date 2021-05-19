92692c720b55e38754d1ce81a953521ff31b6b89
tartakovskyi <vvt2001@ukr.net>
2021-05-19 23:03:29 +0300

    radio buttons

--- src/components/Employee.jsx
+++ src/components/Employee.jsx
@@ -1,20 +1,42 @@
-import React  from 'react'
+import React, { useState }  from 'react'
 
 
 const Employee = ({user:{id, firstName, lastName}}) => {
+
+  const [active, setActive] = useState('0');
+
+  const onChangeValue = e => {
+    setActive(e.target.value)
+  }
 
   return (
     <li className="employee">
       <h4 className="employee__name">{firstName} {lastName}</h4>
       <form>
-        <div className="form-check">
-          <input type="radio" name={`active_${id}`} onChange={e => console.log(e.target.value)} value="0" id={`notActive_${id}`} className="form-check-input" checked />
-          <label htmlFor={`notActive_${id}`} className="form-check-label">not active</label>
-        </div>
-        <div className="form-check">  
-          <input type="radio" name={`active_${id}`} onChange={e => console.log(e.target.value)} value="1" id={`active_${id}`} className="form-check-input"/>
-          <label htmlFor={`active_${id}`} className="form-check-label">active</label>
-        </div>
+        <label className="form-check">
+          <input 
+            type="radio"
+            name={`active_${id}`}
+            value="0"
+            id={`notActive_${id}`}
+            className="form-check-input"
+            onChange={onChangeValue}
+            checked={active === '0'}
+          />
+          <span className="form-check-label">not active</span>
+        </label>
+        <label className="form-check">  
+          <input 
+            type="radio"
+            name={`active_${id}`}
+            value="1"
+            id={`active_${id}`}
+            className="form-check-input"
+            onChange={onChangeValue}
+            checked={active === '1'}
+          />
+          <span className="form-check-label">active</span>
+        </label>
       </form>
     </li>
   )
