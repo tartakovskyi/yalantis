a4fc17d3c278bb59e19f0ffa75f8302fb13526c4
tartakovskyi <vvt2001@ukr.net>
2021-05-25 09:18:23 +0300

    snapshot

--- src/components/employees/__tests__/Employee.test.js
+++ src/components/employees/__tests__/Employee.test.js
@@ -4,21 +4,27 @@ import { AppContext } from "../../App";
 import Employee from "../Employee";
 
 test("Employee shold render correctly", () => {
-	const user = {
-		"id": "5e00928d91e7feaa9872ec08",
-		"firstName": "Yang",
-		"lastName": "Carson",
-		"dob": "2019-02-26T16:52:36.244Z"
-	}
-  const activeUsers = ["5e00928dac694570760105c4","5e00928deee1b67fa4c44a2a","5e00928d1a9ac4bd431ff876","5e00928d4267f8de85992159","5e00928d8d97a843dbb05270","5e00928d3939c957e9c63c2a"]
+  const user = {
+    id: "5e00928d91e7feaa9872ec08",
+    firstName: "Yang",
+    lastName: "Carson",
+    dob: "2019-02-26T16:52:36.244Z",
+  };
+  const activeUsers = [
+    "5e00928dac694570760105c4",
+    "5e00928deee1b67fa4c44a2a",
+    "5e00928d1a9ac4bd431ff876",
+    "5e00928d4267f8de85992159",
+    "5e00928d8d97a843dbb05270",
+    "5e00928d3939c957e9c63c2a",
+  ];
+  const changeUserStatus = () => {};
 
-  const changeUserStatus = () => {}
-	
-  const {container} = render(
-    <AppContext.Provider value={{activeUsers, changeUserStatus}}>
+  const { container } = render(
+    <AppContext.Provider value={{ activeUsers, changeUserStatus }}>
       <Employee user={user} />
     </AppContext.Provider>
-  )
+  );
 
-	expect(container.firstChild).toMatchSnapshot();
+  expect(container.firstChild).toMatchSnapshot();
 });
